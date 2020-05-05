46ab37b890adcb181ac5436f96fa8848a3e2dac1
cahyoardhi <sebastiancahyoardhiiswara@gmail.com>
2020-05-05 11:11:42 +0800

    update program

--- defuzzyfikasi.py
+++ defuzzyfikasi.py
@@ -4,9 +4,13 @@ def fdefuzzyfikasi(inputan):
             kecil = v
         else:
             besar = v
+    
+    tempbesar   = 0
+    tempkecil   = 0
+    x           = 0
+    y           = 0
 
-    tempbesar, tempkecil, x, y = (0 for i in range(4))
-    for i in range(20,91):
+    for i in range(20,90,7):
         if i <= 60:
             tempkecil += i
             x += 1
@@ -14,6 +18,6 @@ def fdefuzzyfikasi(inputan):
             tempbesar += i
             y += 1
     x -=1
-    return((tempkecil * kecil) + (tempbesar * besar)) / ((kecil * x) + (besar * y))
-        #((27+34+41+48+55)*0.2 + (62+69+76+83+90)*0.5) / ((0.5)*5 + (0.2)*5) kasus cahyo
-        #((45 + 50 + 55) * 0.75 + (60+65+70+75+80+85+90) * 0.25 / ((0.75)*3) + (0.25)*7)) kasus dinda        
+    return ((tempkecil * kecil) + (tempbesar * besar)) / ((kecil * x) + (besar * y))
+        #((27+34+41+48+55)*0.2 + (62+69+76+83+90)*0.5) / ((0.5)*5 + (0.2)*5)    kasus 1
+        #((45 + 50 + 55) * 0.75 + (60+65+70+75+80+85+90) * 0.25 / ((0.75)*3) + (0.25)*7)) kasus 2
